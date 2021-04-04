3c12d599896ef9f6b9c9168c3c55fcf58cf668aa
snakajim <snakajim@arm.com>
2021-04-04 22:38:54 +0900

    bug fix

--- scripts/install_docker_ubuntu.sh
+++ scripts/install_docker_ubuntu.sh
@@ -9,16 +9,12 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt -y autoremove
 
 # set environment
-sudo apt install -y\
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+	"deb [arch=arm64] https://download.docker.com/linux/ubuntu \
+	$(lsb_release -cs) \
+	stable"
 sudo apt update
 sudo apt-cache policy docker-ce
 
