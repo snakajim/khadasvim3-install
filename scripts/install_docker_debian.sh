44121d41f95327fc057e87ebb63d4876aca99175
snakajim <snakajim@arm.com>
2021-04-04 17:15:18 +0900

    minor update

--- scripts/install_docker_debian.sh
+++ scripts/install_docker_debian.sh
@@ -15,6 +15,9 @@ sudo apt install \
     gnupg2 \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt-cache policy docker-ce
+
 sudo apt-get update
 # install docker-ce
 sudo apt install -y --no-install-recommends docker-ce
