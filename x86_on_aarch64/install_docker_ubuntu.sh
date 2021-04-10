4448a300efbf41113dda7827da175b6e556cf774
snakajim <satoshi.nakajima@arm.com>
2021-04-10 19:07:59 +0900

    bug fixes.

--- x86_on_aarch64/install_docker_ubuntu.sh
+++ x86_on_aarch64/install_docker_ubuntu.sh
@@ -5,16 +5,16 @@
 # Install docker infrastructure to run x86 container on aarch64
 #
 
-# -----
+# -------------------------------------
 # remove old docker versions
-# -----
+# -------------------------------------
 sudo dpkg --remove --force-remove-reinstreq docker-ce docker-ce-rootless-extras
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt -y autoremove
 
-# -----
+# -------------------------------------
 # set environment and install docker-ce
-# -----
+# -------------------------------------
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
@@ -25,19 +25,21 @@ sudo apt update
 sudo apt-cache policy docker-ce
 sudo apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io
 
-# -----
+# -------------------------------------
 # add users and systemctl
-# -----
-sudo gpasswd -a $USER docker
-sudo chmod 666 /var/run/docker.sock
+# -------------------------------------
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo gpasswd -a $USER docker
+sudo chmod 666 /var/run/docker.sock
+docker images -aq | xargs docker rmi
 docker images 
 docker run hello-world
 
-#
+# -------------------------------------
 # set docker buildx
-#
+# this is optional, you can skip it.
+# -------------------------------------
 
 mkdir -p ${HOME}/.docker/cli-plugins && cd ${HOME}/.docker/cli-plugins && \
  aria2c -x8 https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-arm64 -o docker-buildx && \
@@ -49,30 +51,42 @@ if [ -z "${DOCKER_CLI_EXPERIMENTAL}" ]; then
   echo "export DOCKER_CLI_EXPERIMENTAL=enabled" >> ${HOME}/.bashrc
 fi
 
-cd ${HOME}/tmp && git clone git://github.com/docker/buildx && cd buildx && make install
+cd ${HOME}/tmp && rm -rf buildx && git clone git://github.com/docker/buildx && cd buildx && make install
 
 
-# -----
+# -------------------------------------
+# Enable docker multi arch
+# -------------------------------------
+sudo apt install qemu-user-static
+docker run --rm --privileged multiarch/qemu-user-static:register
+
+# -------------------------------------
 # Dynamic Binary Hardware Injection (DBHI) qemu-user-static (qus)
 # https://github.com/dbhi/qus
-# -----
-sudo apt install qemu-user-static
+# -------------------------------------
 
 # pull images
-docker pull amd64/ubuntu
-docker pull arm64v8/ubuntu
+# Ubuntu 16.04 LTS
+docker pull multiarch/ubuntu-core:amd64-xenial
+docker pull multiarch/ubuntu-core:arm64v8-xenial
+# Ubuntu 18.04 LTS
+docker pull multiarch/ubuntu-core:amd64-bionic
+docker pull multiarch/ubuntu-core:arm64v8-bionic
 # test
 docker system prune -f
-#docker run --rm --privileged aptman/qus -s -- -p amd64
 echo `docker run --rm -t amd64/ubuntu uname -m` | grep x86_64
 echo `docker run --rm -t arm64v8/ubuntu uname -m` | grep aarch64
 
 
-# -----------
+# -------------------------------------
 # build x86_64 docker container on aarch64 linux 
-# -----------
+# -------------------------------------
+docker system prune -f
 sudo apt install subversion -y
 cd ${HOME}/work && svn export  https://github.com/ARM-software/Tool-Solutions/trunk/docker/tensorflow-lite-micro-rtos-fvp
-sed -i 's/FROM ubuntu:/FROM amd64\/ubuntu:/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
-cd ${HOME}/work/tensorflow-lite-micro-rtos-fvp && chmod +x -R * && ./docker_build.sh -c gcc
+# use Ubuntu 16.04 LTS(xenial) instead to avoid libc-bin issue
+sed -i 's/FROM ubuntu:18.04/FROM multiarch\/ubuntu-core:amd64-xenial/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
+#sed -i 's/FROM ubuntu:18.04/FROM multiarch\/ubuntu-core:amd64-bionic/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
+chmod +x -R ${HOME}/work/tensorflow-lite-micro-rtos-fvp/*
+cd ${HOME}/work/tensorflow-lite-micro-rtos-fvp && ./docker_build.sh -c gcc
 
