849ba1bdfe903d484bd5f2385b8c11f8209a655f
snakajim <satoshi.nakajima@arm.com>
2021-04-11 12:24:18 +0900

    use HBHI qus then pass!

--- x86_on_aarch64/install_docker_ubuntu.sh
+++ x86_on_aarch64/install_docker_ubuntu.sh
@@ -43,44 +43,57 @@ docker run hello-world
 # this is optional, you can skip it.
 # -------------------------------------
 
-mkdir -p ${HOME}/.docker/cli-plugins && cd ${HOME}/.docker/cli-plugins && \
- aria2c -x8 https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-arm64 -o docker-buildx && \
- chmod a+x ${HOME}/.docker/cli-plugins/docker-buildx
-
-if [ -z "${DOCKER_CLI_EXPERIMENTAL}" ]; then
-  export DOCKER_CLI_EXPERIMENTAL=enabled
-  echo "# set docker buildx" >> ${HOME}/.bashrc 
-  echo "export DOCKER_CLI_EXPERIMENTAL=enabled" >> ${HOME}/.bashrc
-fi
-
-cd ${HOME}/tmp && rm -rf buildx && git clone git://github.com/docker/buildx && cd buildx && make install
+#mkdir -p ${HOME}/.docker/cli-plugins && cd ${HOME}/.docker/cli-plugins && \
+# aria2c -x8 https://github.com/docker/buildx/releases/download/v0.5.1/buildx-v0.5.1.linux-arm64 -o docker-buildx && \
+# chmod a+x ${HOME}/.docker/cli-plugins/docker-buildx
+#if [ -z "${DOCKER_CLI_EXPERIMENTAL}" ]; then
+#  export DOCKER_CLI_EXPERIMENTAL=enabled
+#  echo "# set docker buildx" >> ${HOME}/.bashrc 
+#  echo "export DOCKER_CLI_EXPERIMENTAL=enabled" >> ${HOME}/.bashrc
+#fi
+#cd ${HOME}/tmp && rm -rf buildx && git clone git://github.com/docker/buildx && cd buildx && make install
 
 
 # -------------------------------------
-# Enable docker multi arch
+# Docker multiarch/qemu-user-static
+# https://hub.docker.com/r/multiarch/qemu-user-static
+#
+# Known issue: 
+# Segmentation Fault at libc-bin installation at 
+# Ubuntu 18.04 and 20.04 on x86_64.
+# To avoid errors, use DBHI qus instead.             
 # -------------------------------------
-sudo apt install qemu-user-static
-docker run --rm --privileged multiarch/qemu-user-static:register
+#sudo apt install -y qemu-user-static
+#docker run --rm --privileged multiarch/qemu-user-static:register --reset
+#docker run --rm --privileged multiarch/qemu-user-static:register
 
 # -------------------------------------
 # Dynamic Binary Hardware Injection (DBHI) qemu-user-static (qus)
 # https://github.com/dbhi/qus
+#
+# This is alternative way to emulate x86_64 on aarch64. 
+# If docker multiarch does not work properly, use this instead. 
 # -------------------------------------
+sudo apt install -y qemu-user-static
+docker run --rm --privileged aptman/qus -- -r
+docker run --rm --privileged aptman/qus -s -- -p x86_64
 
 # pull images
 # Ubuntu 16.04 LTS
 docker pull multiarch/ubuntu-core:amd64-xenial
-docker pull multiarch/ubuntu-core:arm64v8-xenial
+docker pull multiarch/ubuntu-core:arm64-xenial
 # Ubuntu 18.04 LTS
 docker pull multiarch/ubuntu-core:amd64-bionic
-docker pull multiarch/ubuntu-core:arm64v8-bionic
+docker pull multiarch/ubuntu-core:arm64-bionic
 # Ubuntu 20.04 LTS
 docker pull multiarch/ubuntu-core:amd64-focal
-docker pull multiarch/ubuntu-core:arm64v8-focal
+docker pull multiarch/ubuntu-core:arm64-focal
 # test
 docker system prune -f
-echo `docker run --rm -t amd64/ubuntu uname -m` | grep x86_64
-echo `docker run --rm -t arm64v8/ubuntu uname -m` | grep aarch64
+docker run --rm -t multiarch/ubuntu-core:amd64-bionic uname -m
+sleep 10
+docker run --rm -t multiarch/ubuntu-core:arm64-bionic uname -m
+sleep 10
 
 
 # -------------------------------------
@@ -88,12 +101,14 @@ echo `docker run --rm -t arm64v8/ubuntu uname -m` | grep aarch64
 # -------------------------------------
 docker system prune -f
 sudo apt install subversion -y
-cd ${HOME}/work && svn export  https://github.com/ARM-software/Tool-Solutions/trunk/docker/tensorflow-lite-micro-rtos-fvp
-# use Ubuntu 16.04 LTS(xenial) instead to avoid libc-bin issue
-sed -i 's/FROM ubuntu:18.04/FROM multiarch\/ubuntu-core:amd64-xenial/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
-sed -i 's/apt-get -y update/apt-get -y update \&\& apt-get install -y software-properties-common \&\& apt-get -y upgrade/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile 
-#sed -i 's/FROM ubuntu:18.04/FROM multiarch\/ubuntu-core:amd64-bionic/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
+if [ -d ${HOME}/work/tensorflow-lite-micro-rtos-fvp ]; then
+  echo "tensorflow-lite-micro-rtos-fvp already exists."
+else
+  cd ${HOME}/work && svn export  https://github.com/ARM-software/Tool-Solutions/trunk/docker/tensorflow-lite-micro-rtos-fvp
+fi
+# use Ubuntu 16.04 LTS(xenial) if you face libc-bin issue
+sed -i 's/FROM ubuntu:18.04/FROM multiarch\/ubuntu-core:amd64-bionic/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile
+sed -i 's/apt-get -y update/apt-get -y update \&\& apt-get install -y software-properties-common/' ${HOME}/work/tensorflow-lite-micro-rtos-fvp/docker/*.Dockerfile 
 chmod +x -R ${HOME}/work/tensorflow-lite-micro-rtos-fvp/*
-popd && cp ../amd64_bin/xxd ${HOME}/work/tensorflow-lite-micro-rtos-fvp/xxd
 cd ${HOME}/work/tensorflow-lite-micro-rtos-fvp && ./docker_build.sh -c gcc
 
