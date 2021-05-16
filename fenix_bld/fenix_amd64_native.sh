1c87ebc6c2428401160bc88d09abb0dfe6d13221
Ubuntu <ubuntu@ip-172-31-37-210.ap-northeast-1.compute.internal>
2021-05-16 10:07:56 +0900

    minor bug fix

--- fenix_bld/fenix_amd64_native.sh
+++ fenix_bld/fenix_amd64_native.sh
@@ -5,12 +5,23 @@
 # git clone https://github.com/khadas/fenix
 #
 today=`date +%F_%H_%M`
-mkdir -p ${HOME}/work
-cd ${HOME}/work
-if [ ! -d ${HOME}/work/fenix ]; then
+WORK_DIR=${HOME}/work
+if [ ! -d $WORK_DIR ]; then
+  mkdir -p $WORK_DIR
+fi
+
+#
+# git clone fenix env
+#
+cd $WORK_DIR
+if [ ! -d $WORK_DIR/fenix ]; then
   git clone --depth 1 https://github.com/khadas/fenix
 fi
-cd ${HOME}/work/fenix
+
+#
+# config and run
+#
+cd $WORK_DIR/fenix
 #sed -i -e "s/KHADAS_BOARD=VIM1/KHADAS_BOARD=VIM3/" config-template.conf
 source env/setenv.sh -q -s  \
   KHADAS_BOARD=VIM3 \
@@ -25,5 +36,5 @@ source env/setenv.sh -q -s  \
   INSTALL_TYPE_RAW=yes
 # patch in PCIe source?
 # https://forum.khadas.com/t/vim3-ubuntu-kernel-pcie-driver-not-load/6070/7
-make -j`nproc` > ${HOME}/work/make_$today.log 2>&1
-#tail -f ${HOME}/work/make_$today.log
+make -j`nproc` > $WORK_DIR/make_$today.log 2>&1
+ccache -C
