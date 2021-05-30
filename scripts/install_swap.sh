20261b002f403e51d1d2aed797aa7d668c05031a
snakajim <satoshi.nakajima@arm.com>
2021-05-30 21:01:48 +0900

    initial file.

---
+++ scripts/install_swap.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Adding extra 4GByte swap under /swapfile
+#
+
+#
+# check available space under /
+#
+SWAPABLE=`df -h / | grep -e "\/" | awk '{ print $4 }' | sed "s/G//"`
+
+if [ $SWAPABLE -ge "20" ]; then
+  sudo swapon -show | grep swapfile
+  ret=$?
+  if [ $ret -eq "0" ]; then
+    echo "You have already had /swapfile."
+    echo "Program exit."
+    sleep 10
+    exit
+  fi
+  echo "Make 4GByte SWAP under /dev/rootfs."
+else
+  echo "Not enough space left for SWAP under /dev/rootfs."
+  echo "Program exit."
+  sleep 10
+  exit 
+fi
+
+sudo fallocate -l 4G /swapfile
+if [ ! -f /swapfile ]; then
+  echo "/swapfile was not created. Program exit."
+  sleep 10
+  exit
+fi
+
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+SWAPSIZE=`sudo swapon --show | grep /swapfile | awk '{ print $3 }'`
+sudo sh -c \
+  "echo '#/swapfile     none     swap     sw     0 0' > /etc/fstab.swap"
+if [ $SWAPSIZE = "4G" ]; then
+  echo "4GByte SWAP is generated under /swapfile."
+else
+  echo "/swapfile is not generated for SWAP."
+  echo "Program exit."
+  sleep 10
+  exit 
+fi
+
+grep swapfile /etc/fstab
+ret=$?
+if [ $? -eq "1" ]; then
+  sudo sh -c \
+    "cat /etc/fstab.swap >> /etc/fstab"
+  echo "To perpetualize your /swapfile, please update /etc/fstab."
+fi
